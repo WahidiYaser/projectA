cfd5d0fe1b895147fa8e34d33f5362da3c60cb4e
WahidiYaser <112104361+WahidiYaser@users.noreply.github.com>
2022-09-05 20:28:39 +0300

    upD

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,27 @@ for(let i = 0; i < rows.length; i+=1){
     sum += parseInt(cells[1].innerText)
 }
 document.getElementById("sum").innerText = sum + ""
+
+let form = document.getElementById("add-product-form")
+
+form.addEventListener("submit", addProduct)
+
+function addProduct(x){
+    x.preventDefault()
+
+let nameInput = document.getElementById("p-name")
+let priceInput = document.getElementById("p-price")
+
+let row = document.createElement("tr")
+let td1 = document.createElement("td")
+let td2 = document.createElement("td")
+
+td1.innerText = nameInput.value
+td2.innerText = priceInput.value
+
+row.appendChild(td1)
+row.appendChild(td2)
+
+let tBody = document.getElementsByTagName("tbody")
+tBody.appendChild(row)
+}
