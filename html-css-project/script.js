9ff6dc661099aaa70dee62cacd6a839a96b04276
WahidiYaser <112104361+WahidiYaser@users.noreply.github.com>
2022-09-12 20:49:30 +0300

    aAaA

--- html-css-project/script.js
+++ html-css-project/script.js
@@ -1,31 +1,46 @@
-function loadFile(event) {
-    /*var loadFile = function (event) {*/
-    const newImage = document.createElement("img")
+/* function loadFile(event) {
+     ----- var loadFile = function (event) { ----
+    
     newImage = URL.createObjectURL(event.target.files[0]);
 };
 
 let form = document.getElementById("add-photo")
 form.addEventListener("submit", addThePhoto)  
+ function addThePhoto(x) {
+   x.preventDefault() 
+} 
+*/
 
- function addThePhoto() {
-   //x.preventDefault() 
-   const newDiv = document.createElement("div")
-   newDiv.setAttribute("class", "column")
-   const newH3 = document.createElement("h3")
-   const newPara = document.createElement("p")
+document.getElementById("p-picture").addEventListener("change", function () {
+  const reader = new FileReader()
+  reader.addEventListener("load", () => {
+    localStorage.setItem("uploaded-image-url", reader.result)
+  })
+  reader.readAsDataURL(this.files[0])
+})
 
-   newH3.innerText = document.getElementById("p-header").value
-   newPara.innerText = document.getElementById("p-description").value
+let form = document.getElementById("add-photo")
+form.addEventListener("submit", addThePhoto)
 
-   newDiv.appendChild(newH3)
-   newDiv.appendChild(newImage(target.files[0]))
-   newDiv.appendChild(newPara)
-   document.body.querySelector(".container").appendChild(newDiv)
+function addThePhoto(x) {
+  x.preventDefault()
+  const readTheReader = localStorage.getItem("uploaded-image-url")
+  console.log(readTheReader)
+  const newImage = document.createElement("img")
+  newImage.setAttribute("src", readTheReader)
+  const newDiv = document.createElement("div")
+  newDiv.setAttribute("class", "column")
+  const newH3 = document.createElement("h3")
+  const newPara = document.createElement("p")
+
+  newH3.innerText = document.getElementById("p-header").value
+  newPara.innerText = document.getElementById("p-description").value
+
+  newDiv.appendChild(newH3)
+  newDiv.appendChild(newImage)
+  newDiv.appendChild(newPara)
+  document.body.querySelector(".container").appendChild(newDiv)
 }
-
-
-
-
 
 
 
@@ -40,4 +55,9 @@ function upload(){
     var image = new SimpleImage(fileinput);
     image.drawTo(imgcanvas);
   }
+
+
+  onchange="loadFile(event)"
+
+
   */
