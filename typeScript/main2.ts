858c50ae280de5dd8df8104ef2169fa67f3844c5
WahidiYaser <112104361+WahidiYaser@users.noreply.github.com>
2022-09-18 23:29:01 +0300

    aAaA

--- typeScript/main2.ts
+++ typeScript/main2.ts
@@ -19,3 +19,38 @@ console.log("even number: " + even)
 
 // question number two 2
 
+let a = 5;
+let b = 6;
+let result = 0;
+for(let i = 0; i < b; i++){
+    result += a; 
+}
+console.log("Kefel Result " + result);
+
+/////////////////////////////////////////////////////////////////////////
+// question number three 3
+
+let aa = 5;
+let bb = 5;
+let result2 = 1;
+for(let i = 0; i < bb; i++){
+result2 *= aa;
+}
+console.log("Hezka Result " + result2);
+
+/////////////////////////////////////////////////////////////////////////////
+// question number eleven 11
+
+let char_a = "a".charCodeAt(0);
+let char_z = "z".charCodeAt(0);
+let printChar = "";
+for(let char = char_a; char <= char_z; char++){
+printChar = String.fromCharCode(char);
+    console.log(printChar);
+}
+char_a = "A".charCodeAt(0);
+char_z = "Z".charCodeAt(0);
+for(let char = char_a; char <= char_z; char++){
+    printChar = String.fromCharCode(char);
+        console.log(printChar);
+    }
