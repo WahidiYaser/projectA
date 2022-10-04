e422d4bacc1c404a895b8b4634e88f523471d979
WahidiYaser <112104361+WahidiYaser@users.noreply.github.com>
2022-10-04 06:15:56 +0300

    aAaA

---
+++ typeScript/main5.js
@@ -0,0 +1,66 @@
+// // -------------------------------1-----------------------------------
+// /////////////////////////// a
+// let size = 15
+// let arr = [ { age:0, height:0, salary:0 } ]
+// for(let i = 0; i < size; i++){
+//     arr[i] = {
+//         age: Math.round(Math.random() * 27 + 18),
+//         height: parseFloat((Math.random() * 1.9 + 1).toFixed(1)) ,
+//         salary :Math.round(Math.random() * 6000 + 2000)
+//     }
+// }
+// console.log(arr)
+// /////////////////////////// b
+// let sum = 0
+// for(let i = 0; i < arr.length; i++){
+//     for(let j = 0; j < arr.length-1; j++){
+//         let checkA = arr[j].age
+//         let checkB = arr[j+1].age
+//         if(checkA > checkB){
+//             let temp = arr[j]
+//             arr[j] = arr[j+1]
+//             arr[j+1] = temp
+//         }
+//     }
+//         sum += arr[i].age
+// }
+// sum = sum / arr.length
+// console.log(arr)
+// /////////////////////////// c
+// //console.log(sum)
+// console.log(" the youngest man " + arr[0].age)
+// console.log(" the older man " + arr[arr.length-1].age)
+// ////////////////////////// D
+// for(let i = 0; i < arr.length-1; i++){
+//     if(arr[i].salary > 4300 && arr[i].age >= 20 && arr[i].age <= 30){
+//         console.log(arr[i])
+//     }
+// }
+// ////////////////////////// E
+// for(let i = 0; i < arr.length-1; i++){
+//     if(arr[i].age > sum){
+//         console.log(arr[i].salary)
+//     }
+// }
+// // -------------------------------2-----------------------------------
+// let size = 5
+// let arr = [{ age: 0, height: 0, salary: 0, name: "" }]
+// for (let i = 0; i < size; i++) {
+//     arr[i] = {
+//         age: Math.round(Math.random() * 27 + 18),
+//         height: parseFloat((Math.random() * 1.9 + 1).toFixed(1)),
+//         salary: Math.round(Math.random() * 6000 + 2000),
+//         name: makeAName(5)
+//     }
+// }
+// console.log(arr)
+function makeAName(num) {
+    var foundName = "";
+    for (var i = num; i >= 0; i++) {
+        var char = Math.round(Math.random() * 25 + 97);
+        var newChar = String.fromCharCode(char);
+        foundName += newChar;
+    }
+    return foundName;
+}
+console.log(makeAName(7));
